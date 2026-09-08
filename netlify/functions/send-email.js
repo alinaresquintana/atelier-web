3eaf59c78f10bda071cb15d4465c9b22f5fd92f9
alinares <antonio.linares@swissquote.ch>
2026-09-08 15:16:59 +0200

    Set good email

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,7 +1,7 @@
 const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const FROM_ADDRESS = process.env.FROM_EMAIL || 'Atelier Arte Vich <onboarding@resend.dev>';
+const FROM_ADDRESS = process.env.FROM_EMAIL || 'Atelier Arte Vich <info@atelierartvich.com>';
 const REPLY_TO_ADDRESS = 'atelierartevich@gmail.com';
 
 exports.handler = async (event) => {
